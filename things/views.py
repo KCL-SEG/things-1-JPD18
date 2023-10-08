19b7b67f253a587c047831f3cae63dabfeff8c91
Johnathan Dawber <johnathan.dawber@kcl.ac.uk>
2023-10-08 13:45:09 -0700

    fix view

--- things/views.py
+++ things/views.py
@@ -3,4 +3,4 @@ from .forms import ThingForm
 # Create your views here.
 def home(request):
     form = ThingForm()
-    return render(request,'things/add_thing.html',{'form':form})
+    return render(request,'things/home.html',{'form':form})
