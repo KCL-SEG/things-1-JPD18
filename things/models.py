7f9513b9481743fa5447d099f74858c6ed1cec92
Johnathan Dawber <johnathan.dawber@kcl.ac.uk>
2023-10-08 14:11:47 -0700

    remove default values

--- things/models.py
+++ things/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=30, unique=True, blank=False, default='guy')
+    name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.TextField(max_length=120, blank=True)
-    quantity = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)],default =0 )
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
