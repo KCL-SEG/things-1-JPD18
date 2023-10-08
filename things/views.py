22f4be6e3b8ea71efef3f870b8c307eca8f71b42
Johnathan Dawber <johnathan.dawber@kcl.ac.uk>
2023-10-08 14:09:46 -0700

    fix render error

--- things/views.py
+++ things/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .forms import ThingForm
+
 # Create your views here.
 def home(request):
-    form = ThingForm()
-    return render(request,'things/home.html',{'form':form})
+    
+    return render(request,'things/home.html')
